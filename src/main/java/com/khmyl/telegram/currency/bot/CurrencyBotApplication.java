b9d5ae6347b1c0c5d05eda489054c994d7cbafa3
Valiantsin Khmyl <valikhmyl3@mail.ru>
2021-08-02 00:10:46 +0300

    add home endpoint

--- src/main/java/com/khmyl/telegram/currency/bot/CurrencyBotApplication.java
+++ src/main/java/com/khmyl/telegram/currency/bot/CurrencyBotApplication.java
@@ -2,12 +2,18 @@ package com.khmyl.telegram.currency.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CurrencyBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyBotApplication.class, args);
 	}
-
+	@GetMapping("/")
+		String home() {
+		return "Hello World!";
+	}
 }
