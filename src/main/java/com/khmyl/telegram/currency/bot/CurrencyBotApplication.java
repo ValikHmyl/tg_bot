9c2c54ce0dae586b3a0c1c6dd767c2a47ac86fa7
Valiantsin Khmyl <valikhmyl3@mail.ru>
2021-08-02 21:58:23 +0300

    awake job

--- src/main/java/com/khmyl/telegram/currency/bot/CurrencyBotApplication.java
+++ src/main/java/com/khmyl/telegram/currency/bot/CurrencyBotApplication.java
@@ -2,11 +2,13 @@ package com.khmyl.telegram.currency.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class CurrencyBotApplication {
 
    public static void main(String[] args) {
---
+++ src/main/java/com/khmyl/telegram/currency/bot/quartz/job/HerokuAwakeJob.java
@@ -0,0 +1,26 @@
+package com.khmyl.telegram.currency.bot.quartz.job;
+
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@ConditionalOnProperty(name = "isHeroku", havingValue = "true")
+public class HerokuAwakeJob {
+
+   @Value("${app_name}")
+   private String appName;
+
+   @Scheduled(cron = "* */20 * ? * *")
+   public void execute() throws JobExecutionException {
+      WebClient.create()
+               .get()
+               .uri(uriBuilder -> uriBuilder.scheme("http").host("{appName}.herokuapp.com").build(appName))
+               .retrieve()
+               .toEntity(String.class)
+               .block();
+   }
+}
