0537fb877a96e761b5b3b22b7b7107c3ee1ded90
Valiantsin Khmyl <valiantsin.khmyl@ukg.com>
2023-05-11 12:07:29 +0300

    proxy, docker slim

--- src/main/java/com/khmyl/telegram/currency/bot/config/WebClientConfig.java
+++ src/main/java/com/khmyl/telegram/currency/bot/config/WebClientConfig.java
@@ -6,6 +6,7 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.ProxyProvider;
 
 import javax.net.ssl.SSLException;
 import java.time.Duration;
@@ -23,8 +25,13 @@ public class WebClientConfig {
 
    public static final int DEFAULT_TIMEOUT = 5000;
 
+   @Value(value = "${proxy.rb.host}")
+   private String proxyHost;
+
+   @Value(value = "${proxy.rb.port}")
+   private int proxyPort;
+
    @Bean
-   //todo
    public WebClient httpsNbrbWebClient() throws SSLException {
       SslContext context = SslContextBuilder.forClient()
               .trustManager(InsecureTrustManagerFactory.INSTANCE)
@@ -33,6 +40,9 @@ public class WebClientConfig {
       HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(context))
               .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, DEFAULT_TIMEOUT)
               .responseTimeout(Duration.ofMillis(DEFAULT_TIMEOUT))
+              .proxy(proxy -> proxy.type(ProxyProvider.Proxy.HTTP)
+                      .host(proxyHost)
+                      .port(proxyPort))
               .doOnConnected(conn ->
                       conn.addHandlerLast(new ReadTimeoutHandler(DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS))
                               .addHandlerLast(new WriteTimeoutHandler(DEFAULT_TIMEOUT, TimeUnit.MILLISECONDS)));
